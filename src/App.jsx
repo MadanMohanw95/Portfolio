7f737fb28f548fa62cb52543f649e4211608b96f
madanmohanw95 <madhanmohanw95@gmail.com>
2024-05-20 01:12:01 -0500

    Loaded images and created initial portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from './App.module.css'
+
+function App() {
+
+  return (
+    <>
+    <div className={styles.App}>
+      Portfolio
+    </div>
+    </>
+  )
+}
+
+export default App
