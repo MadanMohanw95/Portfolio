1bb8b274b29c650398e122e478588aa51a684f15
madanmohanw95 <madhanmohanw95@gmail.com>
2024-05-20 20:44:59 -0500

    modified main

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import esbuild from 'esbuild';
 
 export default defineConfig({
   plugins: [react()],
   esbuild: {
     loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
+    include: /src\/.*\.jsx?$/, 
     exclude: [],
-  },
+  },// vite.config.js
+  base: './',
 });
