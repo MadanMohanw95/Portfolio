7be60f5ec8548acbeb041de7307d711dd002da3d
madanmohanw95 <madhanmohanw95@gmail.com>
2024-05-20 20:32:47 -0500

    modified config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import esbuild from 'esbuild';
 
 export default defineConfig({
   plugins: [react()],
   esbuild: {
     loader: 'jsx',
-    include: /src\/.*\.jsx?$/,  // Matches .js and .jsx files in the src directory
+    include: /src\/.*\.jsx?$/,
     exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        {
-          name: 'load-js-files-as-jsx',
-          setup(build) {
-            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
-              loader: 'jsx',
-              contents: await fs.promises.readFile(args.path, 'utf8'),
-            }));
-          },
-        },
-      ],
-    },
   },
 });
