9c9975addbb4bcc41397ce70d6f8d1acb3df5c04
madanmohanw95 <madhanmohanw95@gmail.com>
2024-07-19 06:44:29 -0500

    Added experience points

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,9 +9,8 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Madan Mohan Reddy Vanga</h1>
         <p className={styles.description}>
-          Experienced Full Stack Engineer with 4+ years of expertise in JavaScript, React.js, and performance testing frameworks.
-          Demonstrated ability to collaborate with UX designers to create responsive user interfaces, optimize performance, and 
-          enhance user experiences in web and mobile applications!
+          Experienced software engineer with 4+ years of expertise in building scalable applications with JavaScript frameworks like React and Node.js, leveraging ES6 features, Redux, and RESTful APIs. 
+          Proven track record in optimizing systems, integrating diverse technologies, and implementing robust database solutions in healthcare and academic settings.
         </p>
         <a href="mailto:madanmohanreddyvanga@email.com" className={styles.contactBtn}>
           Contact Me
